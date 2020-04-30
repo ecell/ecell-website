1981ce19d1bf304146d7f0b619faeb51b546e09d
kaizu <kwaizu@gmail.com>
2020-04-30 23:57:24 +0900

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -61,3 +61,15 @@ html_show_sourcelink = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_sidebars = {
+    '**': [
+        # 'logo.html',
+        'localtoc.html',
+        # 'globaltoc.html',
+        # 'relations.html',
+        # 'sourcelink.html',
+        'searchbox.html',
+        ]
+    }
+
