883005147ff8e36f484984965b5bb3640563dfb8
kaizu <kwaizu@gmail.com>
2020-05-01 00:51:42 +0900

    Revert

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,6 @@ pygments_style = None
 # import sphinx_rtd_theme
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_theme = "sphinx_rtd_theme"
-# html_theme = "romnnn_sphinx_press_theme"
 html_theme = "press"
 
 html_show_sourcelink = True
